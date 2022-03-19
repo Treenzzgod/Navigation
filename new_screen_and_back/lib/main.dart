059f3e4f702e0d1649c8d5a81a5b3e738827342c
Treenzzgod <gio12619@gmail.com>
2022-03-19 12:53:08 +0700

    fix

--- new_screen_and_back/lib/main.dart
+++ new_screen_and_back/lib/main.dart
@@ -5,12 +5,61 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: hlmfirst(),
+    );
+  }
+}
+
+class hlmfirst extends StatelessWidget {
+  const hlmfirst({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open Route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => RouteKedua(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class RouteKedua extends StatelessWidget {
+  const RouteKedua({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(' Go Back! '),
+        ),
+      ),
     );
   }
 }
